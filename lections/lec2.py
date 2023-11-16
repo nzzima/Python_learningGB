d8bd39d8ea0bb135a96643d921a8cc5b65588437
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-11-16 11:49:04 +0300

    Added some info about copy, union, intersection and difference

--- lections/lec2.py
+++ lections/lec2.py
@@ -61,3 +61,19 @@ for (k,v) in dictionary.items():
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print(dictionary.items())
 print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+# Множества
+a = {1,2,3,5,8}
+b = {2,5,8,13,21}
+c = a.copy()
+print(c)
+u = a.union(b)
+print(u)
+i = a.intersection(b)
+print(i)
+dl = a.difference(b)
+print(dl)
+dr = b.difference(a)
+print(dr)
+q = a.union(b).difference(a.intersection(b))
+print(q)
