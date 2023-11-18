feec6430f78756e7e2f9f1d8a24aaf33edfb0692
Nikita Krylov <nzzima@MacBook-Pro-Nikita.local>
2023-11-18 10:40:06 +0300

    Changed Fibonachi task to better view

--- sem2/task11_whichFibonachi.py
+++ sem2/task11_whichFibonachi.py
@@ -1,20 +1,24 @@
 # Which Fibonachi
 
-m = int(input("Введите число больше 1: "))
-
-f_0 = 0
-f_1 = 1
-f_2 = 1
-n = 3
-while f_2 < m:
-    f_0 = f_1
-    f_1 = f_2
-    f_2 = f_0 + f_1
-    n += 1
-    if m == f_2:
-        print(f"Fibonachi! Index = {n}")
-    elif f_2 > m:
-        if abs(m - f_2) > abs(m - f_1):
-            print(f"Not Fibonachi, but closer to is {f_1}")
-        else:
-            print(f"Not Fibonachi, but closer to is {f_2}")
+try:
+    m = int(input("Enter number > 1: "))
+    f_0 = 0
+    f_1 = 1
+    f_2 = 1
+    n = 3
+    while f_2 < m:
+        f_0 = f_1
+        f_1 = f_2
+        f_2 = f_0 + f_1
+        n += 1
+        if m == f_2:
+            print(f"Fibonachi! Index = {n}")
+        elif f_2 > m:
+            if abs(m - f_2) > abs(m - f_1):
+                print(f"Not Fibonachi, but closer to is {f_1}")
+            elif abs(m - f_2) < abs(m - f_1):
+                print(f"Not Fibonachi, but closer to is {f_2}")
+            else:
+                print(f"Not Fibonachi, but closer to is {f_1} and {f_2}")
+except:
+    print("Incorrect number!")
